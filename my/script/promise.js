482ae11a77f868a9a6cc96686fcab3c220127db6
sakdipat <sakdipat5465@gmail.com>
2022-07-18 21:12:45 +0700

    try to use callback function

--- my/script/promise.js
+++ my/script/promise.js
@@ -1,11 +1,26 @@
 // Promise
 
 const connect = true //เช็คว่าต่อเน็ต
+const url1 = "kong.dev/file.json"
 
 function downloading(url){
-    return new Promise(function(resolve, reject)){
-        if(connect){
-            console.log(``)
-        }
-    }
+    return new Promise(function(resolve, reject){
+        setTimeout(() =>{
+            if(connect){
+                resolve(`โหลด ${url} เรียบร้อย`)
+                // console.log(`โหลด ${url} เรียบร้อย`)
+            }else{
+                reject(`เกิดข้อผิดพลาด`)
+                // console.log(`เกิดข้อผิดพลาด`)
+            }
+        },3000)
+    })
 }
+
+downloading(url1).then(result=>{
+    console.log(result)
+}).catch(err=>{
+    console.log(err)
+}).finally(()=>{
+    console.log(`จบการทำงาน`)
+})
