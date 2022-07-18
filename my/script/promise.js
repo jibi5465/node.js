56321064ab1018c990824e83d9609592401a5ad2
sakdipat <sakdipat5465@gmail.com>
2022-07-18 19:55:42 +0700

    add file .js

---
+++ my/script/promise.js
@@ -0,0 +1,11 @@
+// Promise
+
+const connect = true //เช็คว่าต่อเน็ต
+
+function downloading(url){
+    return new Promise(function(resolve, reject)){
+        if(connect){
+            console.log(``)
+        }
+    }
+}
