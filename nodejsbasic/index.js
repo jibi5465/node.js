985c1480fb283a66d4e5a8c658d9444f095b54d6
sakdipat <sakdipat5465@gmail.com>
2022-07-21 18:01:06 +0700

    study make module

--- nodejsbasic/index.js
+++ nodejsbasic/index.js
@@ -1,3 +1,17 @@
-const mymodules = require('./modules/mymodule')
+// เขียนแบบสากล
+const util = require('./modules/mymodule')
+// เขียนแบบฉบับ function
+const now = require('./modules/mymodule').getCurrentTime
+const plus = require('./modules/mymodule').add
+const format = require('./modules/mymodule').formatNumber
 
-console.log(mymodules.getCurrentTime())
+// เขียนได้ 2 แบบ
+console.log(util.getCurrentTime())  //แบบที่1
+console.log(now())                  //แบบที่2
+
+console.log(util.add(50,100))       //แบบที่1
+console.log(plus(100,50))           //แบบที่2
+
+const number = 50000000
+console.log(util.formatNumber(number)) 
+console.log(format(number))
--- nodejsbasic/modules/mymodule.js
+++ nodejsbasic/modules/mymodule.js
@@ -1,9 +1,21 @@
 // ให้บริการเกี่ยวกับการทำงานต่างๆในโปรแกรม
+
+// ฟังก์ชันเวลา
 function getCurrentTime() {
     return new Date()
 }
 
+// ฟังก์ชันบวกเลข
+function add(x,y){
+    return x+y
+}
+
+function formatNumber(num){
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
 
 
 // ส่งออกคำสั่งไปใช้
 module.exports.getCurrentTime = getCurrentTime
+module.exports.add = add
+module.exports.formatNumber = formatNumber
