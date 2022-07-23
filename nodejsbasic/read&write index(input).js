4970f28d8726cfcedac914eb4547101cfd369b2f
sakdipat <sakdipat5465@gmail.com>
2022-07-23 21:31:37 +0700

    study non-blocking

--- nodejsbasic/read&write index(input).js
+++ nodejsbasic/read&write index(input).js
@@ -11,3 +11,17 @@ console.log("จบการทำงาน")
 const outputText = `Hello Node.js\n${data}\nไฟล์ถูกเขียนเมื่อ${new Date()}`
 fs.writeFileSync("./myfile/input.txt",outputText)
 console.log("เขียนไฟล์เรียบร้อย!")
+
+// Non-blocking
+const nfs = require('fs')
+
+// อ่านไฟล์ input.text
+nfs.readFile("./myfile/input.txt",'utf-8',(err, data) => {
+    if(err) return console.log("เกิดข้อผิดพลาด",err)
+    const outputText = `Hello node.js\n${data}\nไฟล์นี้ถูกเขียนเมื่อ ${new Date()}`
+    fs.writeFile("./myfile/output.txt",outputText,err=>{
+        if(err) return console.log("เกิดข้อผิดพลาด",err)
+        console.log("เขียนไฟล์เรียบร้อย")
+    })
+})
+
